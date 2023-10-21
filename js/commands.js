7356ea8fecccf4080f61b6de273992815826a869
AKmahim <mdmahimhossen134@gmail.com>
2023-10-22 04:05:19 +0600

    add terminal portfolio

---
+++ js/commands.js
@@ -0,0 +1,71 @@
+var youtube = "https://www.youtube.com/fknight/";
+var twitter = "https://www.twitter.com/me-mahim/";
+var linkedin = "https://www.linkedin.com/in/me-mahim/";
+var instagram = "https://www.instagram.com/me.mahim/";
+var github = "https://github.com/AKmahim/";
+var email = 'mailto:me.mahim99@gmail.com';
+var facebook = "https://www.fb.com/mee.mahim/"
+
+whois = [
+  "<br>",
+  "Hey, I'm Mahim!👋",
+  "I'm a Computer Science graduate and Full Stack Web Developer, who builds engaging websites like this one",
+  "and Learning about MLL & AI",
+  "After graduating with a Bachelor's in Computer Science, I worked professionally",
+  "Currently I am working at XR Interactive as a full stact web developer",
+  "<br>"
+];
+
+
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/me-mahim' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/me-mahim' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/AKmahim' + "</a>",
+  'facebook       <a href="' + facebook + '" target="_blank">Facebook/mee.mahim' + "</a>",
+
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "You can checkout my github.. most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Mahim?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  ';;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;',
+  '                                                                                                                 ',
+  '  ▄▄▄▄███▄▄▄▄      ▄████████         ▄▄▄▄███▄▄▄▄      ▄████████    ▄█    █▄     ▄█     ▄▄▄▄███▄▄▄▄   ',
+  ' ▄██▀▀▀███▀▀▀██▄   ███    ███       ▄██▀▀▀███▀▀▀██▄   ███    ███   ███    ███   ███   ▄██▀▀▀███▀▀▀██▄ ',
+  ' ███   ███   ███   ███    █▀        ███   ███   ███   ███    ███   ███    ███   ███▌  ███   ███   ███ ',
+  ' ███   ███   ███  ▄███▄▄▄           ███   ███   ███   ███    ███  ▄███▄▄▄▄███▄▄ ███▌  ███   ███   ███ ',
+  ' ███   ███   ███ ▀▀███▀▀▀           ███   ███   ███ ▀███████████ ▀▀███▀▀▀▀███▀  ███▌  ███   ███   ███ ',
+  ' ███   ███   ███   ███    █▄        ███   ███   ███   ███    ███   ███    ███   ███   ███   ███   ███ ',
+  ' ███   ███   ███   ███    ███       ███   ███   ███   ███    ███   ███    ███   ███   ███   ███   ███ ',
+ ' ▀█    ███   █▀    ██████████        ▀█   ███   █▀    ███    █▀    ███    █▀    █▀     ▀█   ███   █▀  ',
+  '                                                                                                      ',
+  ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
